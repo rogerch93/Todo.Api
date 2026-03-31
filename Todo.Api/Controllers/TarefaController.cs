f05a0497cd45aafc3dc95b9a4edaf9bd9b60e754
Rogerio Chaves <rogerinhochaves@hotmail.com>
2026-03-30 23:53:15 -0300

    Ajuste de controller e configuração de Endpoints na API

---
+++ Todo.Api/Controllers/TarefaController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Todo.Aplication.DTOs;
+using Todo.Aplication.Interfaces;
+using Todo.Domain.Enums;
+
+namespace Todo.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class TarefasController : ControllerBase
+    {
+        private readonly ITarefaService _service;
+
+        public TarefasController(ITarefaService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TarefaDto>> Create([FromBody] CreateTarefaDto dto)
+        {
+            var result = await _service.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<TarefaDto>> GetById(Guid id)
+        {
+            var tarefa = await _service.GetByIdAsync(id);
+            return tarefa == null ? NotFound() : Ok(tarefa);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TarefaDto>>> GetAll()
+            => Ok(await _service.GetAllAsync());
+
+        // === ÚNICO ENDPOINT DE FILTRAGEM (Status + Data de Vencimento) ===
+        [HttpGet("filtrar")]
+        public async Task<ActionResult<IEnumerable<TarefaDto>>> GetFiltered(
+            [FromQuery] StatusTarefa? status,
+            [FromQuery] DateTime? dataInicio,
+            [FromQuery] DateTime? dataFim)
+        {
+            var tarefas = await _service.GetFilteredAsync(status, dataInicio, dataFim);
+            return Ok(tarefas);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] UpdateTarefaDto dto)
+        {
+            await _service.UpdateAsync(id, dto);
+            return NoContent();
+        }
+
+        [HttpPatch("{id:guid}/iniciar")]
+        public async Task<IActionResult> Iniciar(Guid id)
+        {
+            await _service.IniciarAsync(id);
+            return NoContent();
+        }
+
+        [HttpPatch("{id:guid}/concluir")]
+        public async Task<IActionResult> Concluir(Guid id)
+        {
+            await _service.ConcluirAsync(id);
+            return NoContent();
+        }
+
+        [HttpPatch("{id:guid}/reabrir")]
+        public async Task<IActionResult> Reabrir(Guid id)
+        {
+            await _service.ReabrirAsync(id);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _service.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
+
--- Todo.Api/Program.cs
+++ Todo.Api/Program.cs
@@ -1,3 +1,16 @@
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text.Json;
+using Todo.Aplication.Interfaces;
+using Todo.Aplication.Mappings;
+using Todo.Aplication.Services;
+using Todo.Aplication.Validators;
+using Todo.Domain.Interfaces;
+using Todo.Infrastructure.Data;
+using Todo.Infrastructure.Persistence;
+using Todo.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +18,39 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Todo API",
+        Version = "v1",
+        Description = "API de Gestão de Tarefas"
+    });
+});
+
+// EF Core
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// AutoMapper
+builder.Services.AddAutoMapper(typeof(TarefaMappingProfile));
+
+// Dependency Injection - Application
+builder.Services.AddScoped<ITarefaService, TarefaService>();
+
+// Dependency Injection - Infrastructure
+builder.Services.AddScoped<ITarefaRepository, TarefaRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+//Validators
+builder.Services.AddValidatorsFromAssemblyContaining<CreateTarefaValidator>();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    });
 
 var app = builder.Build();
 
